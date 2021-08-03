176a4e8f1f9036784998ca3c81a2f8290349b14e
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-08-03 15:45:33 +0200

    carousel slideshow

--- carousel.js
+++ carousel.js
@@ -15,6 +15,11 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slideshow__section");
     var dots = document.getElementsByClassName("dot");
+    var img = document.getElementsByClassName("dot__img");
+    var text = document.getElementsByClassName("dot__text");
+
+
+
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
@@ -22,7 +27,11 @@ function showSlides(n) {
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
+        img[i].className = img[i].className.replace(" dot__img--active", "");
+        text[i].className = text[i].className.replace(" dot__text--active", "")
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+    img[slideIndex - 1].className += " dot__img--active";
+    text[slideIndex - 1].className += " dot__text--active";
 }
