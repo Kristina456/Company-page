a1e7ce288b6ed3dacbd2aca9655f56877631e999
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-08-04 14:51:15 +0200

    sidebar, header and ilustrations

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,11 +1,9 @@
 function hamburgerMenu() {
     const MENU = document.getElementById("small-nav");
     if (MENU.classList.contains("hide")) {
-        console.log("SHowing menu")
         MENU.classList.remove("hide");
         MENU.classList.add("show");
     } else {
-        console.log("Hiding menu")
         MENU.classList.remove("show");
         MENU.classList.add("hide");
     }
