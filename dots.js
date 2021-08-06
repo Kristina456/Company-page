34291ccc25643dece5e60496061d077d348e9ee7
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-08-06 16:20:56 +0200

    javascript modifications

--- dots.js
+++ dots.js
@@ -1,12 +1,12 @@
 function openParagraf(number) {
-    var x = document.getElementById("popover-" + number);
+    let x = document.getElementById("popover-" + number);
     if (x.style.display === "none") {
         x.style.display = "flex";
     }
 }
 
 function closeParagraf(number) {
-    var x = document.getElementById("popover-" + number);
+    let x = document.getElementById("popover-" + number);
     if (x.style.display === "flex") {
         x.style.display = "none";
     }
