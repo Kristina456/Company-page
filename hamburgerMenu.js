5dfd20bbd968dc42c225c22cde23239eb73a6b66
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-08-16 11:28:32 +0200

    hemburger menu

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,10 +1,18 @@
-function hamburgerMenu() {
-    const MENU = document.getElementById("small-nav");
-    if (MENU.classList.contains("hide")) {
-        MENU.classList.remove("hide");
-        MENU.classList.add("show");
-    } else {
-        MENU.classList.remove("show");
-        MENU.classList.add("hide");
-    }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }
